bdc2964cbecbf4b9a3f54af73a1166d5a2fee629
시훈 <81404026+sihun96@users.noreply.github.com>
2021-12-09 12:33:49 +0900

    Add files via upload

---
+++ CICDtest/src/main/java/com/cicdtest/CicDtestApplication.java
@@ -0,0 +1,13 @@
+package com.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDtestApplication.class, args);
+    }
+
+}
---
+++ CICDtest/src/test/java/com/cicdtest/CicDtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
