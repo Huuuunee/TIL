090d92990328360dfa2daa1735598bb415f25dea
시훈 <81404026+Huuuunee@users.noreply.github.com>
2022-07-30 23:44:54 +0900

    Delete CICDtest directory

--- CICDtest/Scripts/remove_before.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find /opt/codedeploy-agent/deployment-root/asd/* -maxdepth 0 type 'd' | grep -v
-$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/asd/* | sort -t: -n | tail-1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
-sudo rm -rf /home/ec2-user/*
--- CICDtest/Scripts/run_process.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user
-sudo java -jar *.jar > /dev/null 2> /dev/null< /dev/null &
